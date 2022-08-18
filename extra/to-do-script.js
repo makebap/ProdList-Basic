6bd4c616e6afb4264c7e0908f2ceed327bd5378e
makeba <makebalproverbs@gmail.com>
2022-08-18 09:12:13 -0400

    "added edit item"

--- extra/to-do-script.js
+++ extra/to-do-script.js
@@ -16,25 +16,69 @@ for (let link of navItem) {
     })
 }
 
+const validInp = (str) => {
+    if (str.startsWith(' ')){
+        return false;
+    } else {
+        return true;
+    }
+}
+
 //add item
 const addNewForm = document.querySelector('#add-item');
 addNewForm.addEventListener('submit', function(e) {
     const newTaskInput = document.querySelector('#add-to-do');
     const list = document.querySelector('#to-do-list');
     e.preventDefault();
-    const newTask = newTaskInput.value;
-    const newTaskCard = document.createElement('div');
-    newTaskCard.classList.add('to-do-item');
-    newTaskCard.innerText = newTask;
-    list.append(newTaskCard);
-    newTaskInput.value = '';
-    const toDoDisplay = document.querySelector('#unfin-num');
-    let toDoSum = parseInt(toDoDisplay.innerText);
-    toDoSum += 1;
-    toDoDisplay.innerText = toDoSum;
+    if (newTaskInput.value.length != 0 && validInp(newTaskInput.value)){
+        const newTask = newTaskInput.value;
+        const newTaskCard = document.createElement('div');
+        newTaskCard.classList.add('to-do-item');
+        const newTaskForm = document.createElement('form');
+        const newTaskText = document.createElement('input');
+        newTaskText.type = 'text';
+        newTaskText.classList.add('task-input');
+        newTaskText.classList.add('col');
+        newTaskText.classList.add('col-10');
+        newTaskText.value = newTask;
+        newTaskForm.append(newTaskText);
+        const deleteBtn = document.createElement('button');
+        deleteBtn.innerText = 'Delete';
+        deleteBtn.type = 'button';
+        deleteBtn.classList.add('task-delete');
+        deleteBtn.classList.add('col');
+        deleteBtn.classList.add('col-2');
+        newTaskForm.append(deleteBtn);
+        newTaskForm.classList.add('taskCardForm');
+        newTaskForm.classList.add('container');
+        newTaskForm.action = '';
+        newTaskCard.append(newTaskForm);
+        list.append(newTaskCard);
+        newTaskInput.value = '';
+        const toDoDisplay = document.querySelector('#unfin-num');
+        let toDoSum = parseInt(toDoDisplay.innerText);
+        toDoSum += 1;
+        toDoDisplay.innerText = toDoSum;
+    }
 })
 
 //editing and deleting item
+//edit
+const taskForms = document.querySelectorAll('.taskCardForm');
+
+for (let task of taskForms){
+    task.addEventListener('submit', function(e) {
+        e.preventDefault();
+        console.log('submitted');
+    })
+}
+
+
+
 
 //delete
-const listItem = document.querySelector('.to-do-item');
+const list = document.querySelector('#to-do-list');
+
+list.addEventListener('click', function(e) {
+    
+})
